e9095fb8efb7c58fc7f31ef7ae0ba664f0d0579e
Tumer <Tumer@TUMER>
2018-11-25 22:04:40 +0300

    Added project controllers and views.

---
+++ BeautyCenter/Controllers/Report/IncomeReportController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautyCenter.Controllers.Report
+{
+    public class IncomeReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
